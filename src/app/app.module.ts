cd36e02569d50b8f99036d09eb518d9e014bc52c
sudhan <sudhant22@gmail.com>
2021-09-01 17:51:25 -0700

    final commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CommonLayoutComponent } from './layouts/common-layout/common-layout.component';
-import { LoginComponent } from './components/login/login.component';
-
+ //import { LoginComponent } from './view/login/login.component';
+import{AdminModule} from './admin/admin.module';
 import { routing } from './app.routing';
 import { AdminLoginComponent } from './components/admin-login/admin-login.component';
 import { PatientLoginComponent } from './components/patient-login/patient-login.component';
@@ -14,13 +14,13 @@ import { PharmacistLoginComponent } from './components/pharmacist-login/pharmaci
   declarations: [
     AppComponent,
     CommonLayoutComponent,
-    LoginComponent,
+     //LoginComponent,
     AdminLoginComponent,
     PatientLoginComponent,
     PharmacistLoginComponent
   ],
   imports: [
-    BrowserModule, routing
+    BrowserModule,AdminModule, routing
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,11 @@
 import { RouterModule, Routes } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
-import { LoginComponent } from './components/login/login.component';
+
 import { AdminLoginComponent } from './components/admin-login/admin-login.component';
 import { PatientLoginComponent } from './components/patient-login/patient-login.component';
 import { PharmacistLoginComponent } from './components/pharmacist-login/pharmacist-login.component';
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
+  //  { path: 'login', component: LoginComponent },
   { path: 'adminlogin', component: AdminLoginComponent },
   { path: 'patientlogin', component: PatientLoginComponent },
   { path: 'pharmacistlogin', component: PharmacistLoginComponent }
